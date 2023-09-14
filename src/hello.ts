ea00df37da670ec759f4b4132f05239f2f17e812
Kim, Allen <allen.kim@ontariohealth.ca>
2023-09-14 18:20:22 -0400

    wip

--- src/hello.ts
+++ src/hello.ts
@@ -1,10 +1,12 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import crypto from 'crypto';
 
 export const helloHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
   const name = event.queryStringParameters?.name || 'you';
+  const hash = crypto.createHash('md5').update(name).digest('hex');
 
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'hello ' + name}),
+    body: JSON.stringify({ message: `hello ${name}, ${hash}`}),
   };
 };
